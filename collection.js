4ff169754a2caf152b2b4be7907a95898a08aa77
rajesh-sathya <rajeshsathya0304@gmail.com>
2025-09-16 16:49:22 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,28 @@
+var ProductContainer=document.getElementById("products")
+var search=document.getElementById("search")
+var productlist=ProductContainer.querySelectorAll("div")
+
+
+
+search.addEventListener("keyup",function(event){
+    var enteredValue=event.target.value.toUpperCase()
+
+
+     for(count=0;count<productlist.length;count=count+1)
+
+        {
+            var productname= productlist[count].querySelector("p").textContent
+     
+        if(productname.toUpperCase().indexOf(enteredValue)<0)
+        {
+            productlist[count].style.display="none"
+
+        }
+
+        else{
+            productlist[count].style.display="block"
+        }
+           }
+
+
+})
---
+++ index.js
@@ -0,0 +1,16 @@
+var sidenav=document.querySelector(".side-navbar")
+
+
+
+function showNavbar()
+{
+    sidenav.style.left="0"
+
+}
+
+function closenavbar()
+{
+
+    sidenav.style.left="-60%"
+
+}
